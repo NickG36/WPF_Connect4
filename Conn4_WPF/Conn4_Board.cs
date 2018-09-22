42c01721cae8e0cf2cfbaf84835e7c7545c4a3fd
NickG36 <Nick_Gliddon@hotmail.com>
2018-09-22 09:21:53 +0100

    First version of initial files

---
+++ Conn4_WPF/Conn4_Board.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Conn4Board
+/// </summary>
+/// 
+namespace Conn4_WPF
+{
+
+    public class Conn4Board
+    {
+        public const int NUM_ROWS = 6;
+        public const int NUM_COLS = 7;
+
+        public Conn4Board()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+    }
+
+}
---
+++ Conn4_WPF/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Conn4_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Button[,] grid = new Button[Conn4Board.NUM_ROWS, Conn4Board.NUM_COLS];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // TO DO: Setup grid
+        }
+
+        private void colBtnClicked(int col_idx)
+        {
+            System.Console.WriteLine(col_idx + " pressed ");
+        }
+
+        private void btnCol1_Click(object sender, RoutedEventArgs e)
+        {
+            colBtnClicked(1);
+        }
+
+        private void btnCol2_Click(object sender, RoutedEventArgs e)
+        {
+            colBtnClicked(2);
+        }
+
+        private void btnCol3_Click(object sender, RoutedEventArgs e)
+        {
+            colBtnClicked(3);
+        }
+
+        private void btnCol4_Click(object sender, RoutedEventArgs e)
+        {
+            colBtnClicked(4);
+        }
+
+        private void btnCol5_Click(object sender, RoutedEventArgs e)
+        {
+            colBtnClicked(5);
+        }
+
+        private void btnCol6_Click(object sender, RoutedEventArgs e)
+        {
+            colBtnClicked(6);
+        }
+
+        private void btnCol7_Click(object sender, RoutedEventArgs e)
+        {
+            colBtnClicked(7);
+        }
+    }
+}
